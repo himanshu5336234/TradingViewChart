e1455cef7501b027604a416fe8c9e388c88b37e6
himanshu5336234 <88225730+himanshu5336234@users.noreply.github.com>
2025-04-01 20:01:28 +0530

    Update baseUrl.js

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -1,8 +1,6 @@
 const params = new Proxy(new URLSearchParams(window.location.search), {
 	get: (searchParams, prop) => searchParams.get(prop),
 });
-
-const token  = params.token
 const env = params.env;
 
 export const deploymentEnv = {
@@ -17,67 +15,53 @@ export const deploymentEnv = {
 
 export const getBaseURL = () => {
   let binanceBaseUrl;
-  let densityBaseUrl;
   let binanceWsBase;
-  let appUrl;
+
   switch (env) {
     case deploymentEnv.PROD:
       binanceBaseUrl = "https://fapi.binance.com";
-      densityBaseUrl = "https://api-prod.density.exchange";
       binanceWsBase = "wss://fstream.binance.com/stream";
-      appUrl = "https://app.density.exchange";
+
       break;
     case deploymentEnv.STAGING:
       case deploymentEnv.UAT:
       binanceBaseUrl = "https://fapi.binance.com";
-      densityBaseUrl = "https://api-prod.density.exchange";
+
       binanceWsBase = "wss://fstream.binance.com/stream";
-      appUrl = "https://staging.density.exchange";
       break;
       
     case deploymentEnv.DEMO:
       binanceBaseUrl = "https://testnet.binancefuture.com";
-      densityBaseUrl = "https://dev-api.densityexchange.org";
+
       binanceWsBase = "wss://stream.binancefuture.com/stream";
-      appUrl = "https://demo.density.exchange";
+
       break;
     case deploymentEnv.SF:
       binanceBaseUrl = "https://testnet.binancefuture.com";
-      densityBaseUrl = "https://dev-api.densityexchange.org";
+
       binanceWsBase = "wss://stream.binancefuture.com/stream";
-      appUrl = "https://sf-app.pahal.cloud";
+
       break;
     case deploymentEnv.TEST:
       binanceBaseUrl = "https://testnet.binancefuture.com";
-      densityBaseUrl = "https://dev-api.densityexchange.org";
+
       binanceWsBase = "wss://stream.binancefuture.com/stream";
-      appUrl = "https://test-app.pahal.cloud";
       break;
-    // case deploymentEnv.UAT:
-    //   binanceBaseUrl = "https://testnet.binancefuture.com";
-    //   densityBaseUrl = "https://dev-api.densityexchange.org";
-    //   binanceWsBase = "wss://stream.binancefuture.com/stream";
-    //   appUrl = "https://uat-app.pahal.cloud";
-    //   break;
     case deploymentEnv.DEV:
       binanceBaseUrl = "https://testnet.binancefuture.com";
-      densityBaseUrl = "https://dev-api.densityexchange.org";
       binanceWsBase = "wss://stream.binancefuture.com/stream";
-      appUrl = "https://dev-app.pahal.cloud";
       break;
     case deploymentEnv.LOCAL:
     default:
-      binanceBaseUrl = "https://testnet.binancefuture.com";
-      densityBaseUrl = "https://dev-api.densityexchange.org";
-      binanceWsBase = "wss://stream.binancefuture.com/stream";
-      appUrl = "https://sf-app.pahal.cloud";
+      binanceBaseUrl = "https://fapi.binance.com";
+      binanceWsBase = "wss://fstream.binance.com/stream";
       break;
     }
+    
     return {
       binanceBaseUrl,
-      densityBaseUrl,
       binanceWsBase,
-      appUrl
+ 
     };
 
 }
