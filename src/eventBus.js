e078a532a8620539bc43f5dfa5fd9fc65180034d
himanshu5336234 <88225730+himanshu5336234@users.noreply.github.com>
2025-04-01 20:02:44 +0530

    Create eventBus.js

---
+++ src/eventBus.js
@@ -0,0 +1,20 @@
+export class EventBus {
+    constructor() {
+      this.events = {};
+    }
+  
+    subscribe(event, callback) {
+      if (!this.events[event]) {
+        this.events[event] = [];
+      }
+      this.events[event].push(callback);
+    }
+  
+    publish(event, data) {
+      if (this.events[event]) {
+        this.events[event].forEach(callback => callback(data));
+      }
+    }
+  }
+  
+  export const eventBus = new EventBus();
